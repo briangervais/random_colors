6979a3e7fc16589dd9f945092cd9df7fc661bd2c
Brian Gervais <brian@atendesigngroup.com>
2017-04-13 23:14:49 -0600

    New lightness controls.  Updated styles and js

--- src/js/random_hsla.js
+++ src/js/random_hsla.js
@@ -2,18 +2,28 @@ var $ = require('jquery');
 
 $(document).ready(function() {
 
+  // Global S,L,A values
   var sG = '50' + '%'; // Saturation
   var lG = '50' + '%'; // Lightness
   var aG = '1'; // Alpha
 
-  // Random H value (hsla)
+  // Random H value
   // between 0-360
-  function randomH(h) {
+  function randomH() {
     // Get random number between 0 - 360
     var h = Math.floor(Math.random() * 360) + 1;
-    // var s = '50' + '%';
-    // var l = '50' + '%';
-    // var a = '1';
+    var s = sG;
+    var l = lG;
+    var a = aG;
+    var hsla = h + ', ' + s + ', ' + l + ', ' + a;
+
+    // Return HSLA value
+    return hsla;
+  }
+
+  function resetHSLA() {
+    $('input[type=range]').val();
+    var h = $('input[type=range]').val();
     var s = sG;
     var l = lG;
     var a = aG;
@@ -24,11 +34,7 @@ $(document).ready(function() {
   }
 
   function opposite(color) {
-    // Get random number between 0 - 360
     var h = color - 180;
-    // var s = '50' + '%';
-    // var l = '50' + '%';
-    // var a = '1';
     var s = sG;
     var l = lG;
     var a = aG;
@@ -39,11 +45,7 @@ $(document).ready(function() {
   }
 
   function triad1(color) {
-    // Get random number between 0 - 360
     var h = color - 120;
-    // var s = '50' + '%';
-    // var l = '50' + '%';
-    // var a = '1';
     var s = sG;
     var l = lG;
     var a = aG;
@@ -54,11 +56,7 @@ $(document).ready(function() {
   }
 
   function triad2(color) {
-    // Get random number between 0 - 360
     var h = color - 240;
-    // var s = '50' + '%';
-    // var l = '50' + '%';
-    // var a = '1';
     var s = sG;
     var l = lG;
     var a = aG;
@@ -74,29 +72,17 @@ $(document).ready(function() {
   var rc3 = triad1(rc1.substr(0, rc1.indexOf(',')));
   var rc4 = triad2(rc1.substr(0, rc1.indexOf(',')));
 
-  console.log(rc1, rc2);
-
   // Add Class to Body #1
   // TODO: Move to init function
   $('body').addClass('loaded');
 
   //
   // Set CSS of Elements
-  //
+  // TODO: Move to init function
 
   $('body').css({
     'background': 'hsla(' + rc1 + ')'
   });
-
-  // $('.vein:first-of-type').css({
-  //   background: rc5
-  // });
-  //
-  // $('.vein:last-of-type').css({
-  //   background: rc6
-  // });
-  //
-
 
   // Dog
   $('text').css({
@@ -116,4 +102,39 @@ $(document).ready(function() {
   $('color:nth-of-type(3)').css({
     'background': 'hsla(' + rc4 + ')'
   })
+
+  // Listener Function
+  // TODO: Move to Listener function
+  $('input[type=range]').on('change', function() {
+    // console.log($(this).val());
+
+    // Get HSLA based value of input
+    var rc5 = resetHSLA();
+    var rc6 = opposite(rc5.substr(0, rc5.indexOf(',')));
+    var rc7 = triad1(rc5.substr(0, rc5.indexOf(',')));
+    var rc8 = triad2(rc5.substr(0, rc5.indexOf(',')));
+
+    $('body').css({
+      'background': 'hsla(' + rc5 + ')'
+    });
+
+    // Dog
+    $('text').css({
+      'background': 'hsla(' + rc6 + ')',
+      'color': 'hsla(' + rc5 + ')'
+    });
+
+
+    $('color:nth-of-type(1)').css({
+      'background': 'hsla(' + rc5 + ')'
+    })
+
+    $('color:nth-of-type(2)').css({
+      'background': 'hsla(' + rc7 + ')'
+    })
+
+    $('color:nth-of-type(3)').css({
+      'background': 'hsla(' + rc8 + ')'
+    })
+  });
 });
